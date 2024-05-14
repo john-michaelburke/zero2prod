85384d04c4ac15244ad01a3eead5d27a4490f420
John Michael Burke <johnmichael.burke@swift-nav.com>
2024-05-13 17:26:08 -0700

    Done with chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use zero2prod::run;
-
+use sqlx::PgPool;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
-    run(listener)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connec to Postgres.");
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
