89e033e20ee587426200d5f536908fa324d60623
John Michael Burke <johnmichael.burke@swift-nav.com>
2024-05-13 17:50:51 -0700

    not sure why ci didnt work

--- src/routes/greet.rs
+++ src/routes/greet.rs
@@ -2,5 +2,5 @@ use actix_web::{HttpRequest, Responder};
 
 pub async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    format!("Hello  {}!", &name)
 }
